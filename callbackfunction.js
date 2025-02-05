7afcde5827fe02ea421f4d8db7f9f01e88dcd274
laxmanghimire7 <ghimirelaxman2155@gmail.com>
2025-02-05 12:42:52 +0545

    foreach, reduce, map, filter: method

--- callbackfunction.js
+++ callbackfunction.js
@@ -131,3 +131,245 @@ let newLakxh=product.map((hellos)=>{
 })
 console.log(newLakxh);
 
+// Questions
+//1. Write a function that filters out even numbers from an array.
+const even=[1,2,3,4,5,6,7,8,9,10] 
+let newEven=even.forEach((num)=>{
+    if(num%2==0){
+        console.log(num);
+    }
+});
+
+//2. Filter an array to include only odd numbers. 
+const odd=[1,2,3,4,5,6,7,8,9,10]
+let newOdd=odd.filter((numb)=>{
+    if(numb%2!=0){
+        console.log(numb);
+    }
+})
+
+//3. Given an array of strings, filter out strings that have more than 3 characters. 
+const string=["laxman", "he", "hi", "hlo","ram","rame"]
+const newString=string.map((back)=>{
+    if(back.length>3){
+        console.log(back);
+    }
+});
+
+//4. Write a function that filters out negative numbers from an array. 
+const negative=[1,0,-23,-1,7,8]
+let newNegative=negative.map((lakxh)=>{
+    if(lakxh<0){
+        console.log(lakxh);
+    }
+})
+
+//5. Given an array of boolean values, filter out the `true` values. 
+const boolean=[true, false, false, true, true]
+let newBoolean=boolean.filter((hi)=>{
+    if(hi===false){
+        console.log(hi);
+    }
+})
+
+//6. Filter an array of strings to include only those that contain the letter 'a'. 
+const contain=["laxman", "bisAl", "hello", "ram"]
+let newContain=contain.map((hee)=>{
+     if(hee.includes("a") || hee.includes("A")){
+         console.log(hee);
+     }
+})
+
+//7. Filter an array of numbers to return only those greater than 5. 
+const greater=[1,2,3,4,5,6,7,8,9,10]
+let newGreater=greater.filter((be)=>{
+    if(be>5){
+        return be;
+    }
+})
+console.log(newGreater);
+
+
+//filter 
+let ar=[1,2,3,4,5,6]
+const filterAr=ar.filter((jii)=>{
+    return jii %2==0;
+})
+console.log(filterAr);
+
+//reduce to make a single value......to gain sum..... 
+let a=[1,2,3,4,5,6]
+const val=a.reduce((acc,y)=>{
+    return acc+y;
+},0)
+console.log(val);
+
+
+let arrr=[1,2011,3000,4121,50,90]
+const vall=arrr.filter((xee)=>{
+    return xee>90; 
+})
+let valle=vall.reduce((acc,h)=>{
+    return acc+h;
+})
+console.log(valle);
+
+
+//spread operator(...)
+const spread=[1,2,3,4,5,6]
+let newSpread=[122,...spread,7]
+console.log(newSpread);
+
+
+//rest operator(...) especially used with function......
+
+function jin(...a){
+    let add1=a.reduce((acc,items)=>{
+        return acc+items;
+    },0)
+    return add1;
+}
+const m=jin(1,2,1212,121,212,212,1,22,1,21,21,2,12,1)
+console.log(m);
+
+
+//destructuring.........
+const lala=[1,12,34,756]
+const [f,b,c,d]=lala
+console.log(c);
+
+//destructuring using ...rest operator
+const hoho=[1,2,3,4,5,6,7,8,9,0,]
+const [v,h,...q]=hoho
+console.log(v);
+console.log(q);
+
+// 
+const obj={
+    name:"Lakxh",
+    age:21,
+    location:"Kathmandu"
+}
+const {name, age, location}=obj;
+console.log(name);
+console.log(age);
+console.log(location);
+
+//
+
+let userDatan = [
+    {
+        name:"Lakxh",
+        ages:20,
+        location:"Kathmandu"
+    },
+    {
+        name:"Sam",
+        ages:23,
+        location:"Kathmandu"
+    },
+    {
+        name:"Ram",
+        ages:26,
+        location:"Kathmandu"
+    },
+    {
+        name:"Jivit",
+        ages:28,
+        location:"Kathmandu"
+    }
+]
+
+let newUser=userDatan.reduce((ben,ten)=>{
+    return ben+ten.ages;
+},0)
+console.log(newUser);
+
+
+//8. Write a function that filters out empty strings from an array. 
+let empty=["Ram", "Laxman", "","Jivit"]
+let newEmpty=empty.filter((you)=>{
+    if(you!=""){
+        return you;
+    }
+})
+console.log(newEmpty);
+
+//9. Write a function that maps an array of numbers to an array where each number is doubled. 
+let double=[1,2,3,4,5,6,7,8,9,10]
+let newDaouble=double.map((ers)=>{
+    return ers*2;
+})
+console.log(newDaouble);
+
+//10. Convert an array of numbers into an array of strings using `map`. 
+let strings=[1,2,3,4,5,6,7]
+let newStrings=strings.map((hjhj)=>{
+    return hjhj.toString();
+})
+console.log(newStrings);
+
+//11. Write a function that converts an array of strings to uppercase using `map`. 
+let functiong=["ram", "laxman","saru","saru"]
+let newfunctiong=functiong.map((kjkj)=>{
+    return kjkj.toUpperCase();
+})
+console.log(newfunctiong);
+
+//12. Given an array of numbers, map it to an array of `true` (if the number is positive) or `false` (if the number is negative). 
+let numbe=[1,2,3,-1,-2,-3]
+let newNumbe=numbe.map((nbnb)=>{
+    if(nbnb>0){
+        return true;
+    }
+    else{
+        return false;
+    }
+})
+console.log(newNumbe);
+
+
+//13. Write a function that adds 1 to each number in an array using `map`. 
+let add=[1,2,3,4,5,6,7,8,9,10]
+let newAdd=add.map((addition)=>{
+    return addition+1;
+})
+console.log(newAdd);
+
+//14. Given an array of strings, return an array of the first letter of each string using `map`.
+let letter=["Ram", "Laxman", "Saru"]
+let newLetter=letter.map((baka)=>{
+    return baka.slice(0,1) //charAt(0), baka[0]....
+
+})
+console.log(newLetter);
+
+//15. Convert an array of strings to an array of their lengths using `map`. 
+let lenth=["Laxman", "Ram", "Bishal"]
+let newLenth=lenth.map((yuou)=>{
+    return yuou.length;
+})
+console.log(newLenth);
+
+//16. Write a function that squares each number in an array using `map`.
+let square=[2,3,4,5,6,6,7,78,8,6,5,4,4,4,9]
+let newSquare=square.map((van)=>{
+      return van**2;
+})
+console.log(newSquare);
+
+//17. Use `forEach` to log each element of an array to the console. 
+let consolge=["Ram", "Hari"]
+let newConsolge=consolge.forEach((ash)=>{
+    console.log(ash);
+})
+
+//18. Write a function that uses `forEach` to calculate the sum of an array of numbers. 
+let sumn=[1,2,3,4,5,6]
+let sano=0;
+let newSumn=sumn.forEach((oop)=>{
+    sano=sano+oop;
+})
+console.log(sano);
+
+
